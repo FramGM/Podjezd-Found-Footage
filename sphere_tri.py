6954be142a8e792ce0821be222c182f8663fd002
FramGM <71141393+FramGM@users.noreply.github.com>
2026-08-04 15:29:11 +0300

    Delete sphere_tri.py

--- sphere_tri.py
+++
@@ -1,5 +0,0 @@
-
-# A simple python script to verify my sphere-triangle math before writing C++
-def closest_point_on_triangle(p, a, b, c):
-    # This is a bit complex. Maybe there's an easier approach for AABB-Triangle?
-    pass
